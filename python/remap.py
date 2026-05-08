25d49158a61fb90d80b006e11de15fc4748bb19a
Conor Foran <foranconor@gmail.com>
2026-05-09 00:33:57 +1200

    toolchage stuff

--- python/remap.py
+++ python/remap.py
@@ -1,5 +1,24 @@
+import emccanon
+from interpreter import INTERP_OK
+
 def m6_prolog(self, **words):
-    self.params["next_pocket"]    = float(self.selected_pocket)
-    self.params["current_pocket"] = float(self.current_pocket)
-    self.params["next_tool"]      = float(self.selected_tool)
-    return 0
+    try:
+        if self.selected_pocket < 0:
+            return "T%d: not in tool table" % self.selected_tool
+        self.params["tool_in_spindle"] = float(self.current_pocket)
+        self.params["current_pocket"]  = float(self.current_pocket)
+        self.params["next_pocket"]     = float(self.selected_pocket)
+        self.params["next_tool"]       = float(self.selected_tool)
+        return INTERP_OK
+    except Exception as e:
+        return str(e)
+
+def m6_epilog(self, **words):
+    try:
+        if self.return_value > 0.5:
+            emccanon.CHANGE_TOOL(self.selected_pocket)
+            self.set_tool_parameters()
+            return INTERP_OK
+        return "tool_change returned %.1f, expected positive" % self.return_value
+    except Exception as e:
+        return str(e)
